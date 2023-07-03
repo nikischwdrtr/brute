a78f9cfdc659a6436d3985d998856ec151ca01eb
JebemtiMaiku <niki.iff@hotmail.com>
2023-07-03 19:02:14 +0200

    home button start

--- brute.sh
+++ brute.sh
@@ -11,6 +11,7 @@ echo -e " ))~~~~~~~~~~~~~(( "
 echo -e "(( > brute 117 < ))"
 echo -e " )).............(( "
 echo -e "(((<-._.-'<-._.-)))"
+adb shell input keyevent 3
 adb shell input keyevent 82
 adb shell input swipe 407 1211 378 85
 while true

